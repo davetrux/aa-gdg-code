8d274d6a40079a93261c46f667fece4e9eaa7b35
David Truxall <davetrux@gmail.com>
2013-11-17 17:23:05 -0500

    Initial transfer

---
+++ BadDroid/src/com/devfest/example/Ouch.java
@@ -0,0 +1,85 @@
+package com.devfest.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+public class Ouch extends Activity {
+
+    private Button _uiButton;
+    private Button _blockButton;
+    private Button _asyncButton;
+
+    private TextView _uiText;
+    private TextView _resultText;
+
+    private Person _data;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        _uiText = (TextView) findViewById(R.id.uiTestText);
+        _resultText = (TextView) findViewById(R.id.resultText);
+
+        _uiButton = (Button) findViewById(R.id.testButton);
+        _uiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence temp = _uiText.getText();
+                if("Two".equalsIgnoreCase(_uiText.getText().toString())) {
+                    _uiText.setText("One");
+                } else {
+                    _uiText.setText("Two");
+                }
+            }
+        });
+
+        _blockButton = (Button) findViewById(R.id.blockButton);
+        _blockButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callService("http://devfestdetroit.appspot.com/api/name");
+            }
+        });
+
+        _asyncButton = (Button) findViewById(R.id.asyncButton);
+        _asyncButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                _resultText.setText("");
+                AsyncWeb async = new AsyncWeb(_resultText);
+                async.execute("http://devfestdetroit.appspot.com/api/name");
+
+            }
+        });
+    }
+
+    private void callService(String url) {
+
+        _resultText.setText("");
+
+        WebHelper http = new WebHelper();
+
+        try {
+            String result = http.getHttp(url);
+
+            Gson parser = new Gson();
+
+            _data = parser.fromJson(result, Person.class);
+
+            _resultText.setText(_data.getFirstName() + " " + _data.getLastName());
+
+        } catch (IOException e) {
+            _resultText.setText("Error");
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-aa-gdg-code
+DevFest2013
 ===========
 
-Code from my presentation at Ann Arbor Google Developers Group
+Android Projects for my talk at Michigan Google Developers DevFest 2013
+
+BadDroid is an example of how not to consume web services.
+
+ServiceConsumer is a completed example of the concepts from the talk.
+
+Starter is a template project with many of the parts already added as a starting point for our coding exercise.
+
+[Slides for the talk](https://docs.google.com/presentation/d/1hyWLuk-tPb13W_HbVCD2uE9Hv4QHbjTiqtUgIinIwaw/edit?usp=sharing)
