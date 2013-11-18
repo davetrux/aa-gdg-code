f1666e6a70d3f25bf9f02005dd4486ee73657af5
David Truxall <davetrux@gmail.com>
2013-11-17 20:14:01 -0500

    Updated to use HttpUrlConnection

--- Starter/src/com/devfest/starter/WebHelper.java
+++ Starter/src/com/devfest/starter/WebHelper.java
@@ -4,17 +4,13 @@ package com.devfest.starter;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.AbstractHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.commons.io.IOUtils;
+import android.text.TextUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class WebHelper {
-    AbstractHttpClient mClient = new DefaultHttpClient();
 
     public static boolean isOnline(Context ctx){
         ConnectivityManager manager =  (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -22,17 +18,69 @@ public class WebHelper {
         return info != null && info.isConnected();
     }
 
+    /**
+     * Performs a GET request on a URL
+     * @param url The URL
+     * @return THe result as a string
+     * @throws IOException
+     */
     public String getHttp(String url) throws IOException {
+        return getHttp(url, "GET", "");
+    }
 
-        //Set up the HTTP calls
-        HttpGet request = new HttpGet(url);
-        HttpResponse response = mClient.execute(request);
+    /**
+     * A more verbose version of the function to handle both POST and GET
+     * @param url The resource URL
+     * @param method GET or POST
+     * @param input HTTP body for posting
+     * @return the result as a string
+     * @throws IOException
+     */
+    public String getHttp(String url, String method, String input) throws IOException {
 
-        //Get the data from the body of the response
-        InputStream stream = response.getEntity().getContent();
-     	byte byteArray[] = IOUtils.toByteArray(stream);
-     	String json = new String( byteArray );
-     	stream.close();
-        return json;
+        OutputStream os = null;
+        BufferedReader in = null;
+
+        try {
+            final URL networkUrl = new URL(url);
+            final HttpURLConnection conn = (HttpURLConnection) networkUrl.openConnection();
+            conn.setRequestMethod(method);
+
+            if (!TextUtils.isEmpty(input)) {
+                //Create HTTP Headers for the content length and type
+                conn.setFixedLengthStreamingMode(input.getBytes().length);
+                conn.setRequestProperty("Content-Type", "application/json");
+                //Place the input data into the connection
+                conn.setDoOutput(true);
+                os = new BufferedOutputStream(conn.getOutputStream());
+                os.write(input.getBytes());
+                //clean up
+                os.flush();
+            }
+
+            final InputStream inputFromServer = conn.getInputStream();
+
+            in = new BufferedReader(new InputStreamReader(inputFromServer));
+            String inputLine;
+            StringBuffer json = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                json.append(inputLine);
+            }
+
+            return json.toString();
+
+        } catch (Exception ex) {
+            //Really should log something here
+            return "";
+        } finally {
+            //clean up
+            if (in != null) {
+                in.close();
+            }
+            if (os != null) {
+                os.close();
+            }
+        }
     }
 }
